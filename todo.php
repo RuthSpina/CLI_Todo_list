481ecc48f669205399cc1dcc26b0e31cd560b3dd
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-08-20 09:42:30 -0500

    updated list with sort function

--- todo.php
+++ todo.php
@@ -8,7 +8,7 @@ $items = array();
 
  // Get STDIN, strip whitespace and newlines, 
  // and convert to uppercase if $upper is true
- function get_input($upper = FALSE) 
+function get_input($upper = FALSE) 
 {
 
     if ($upper) {
@@ -26,10 +26,9 @@ $items = array();
 
 function list_items($list)
 {
-    $list_string = '';
-    //$num = 1;
+    $list_string = '';  
 
-    foreach ($list as $num => $value) {
+foreach($list as $num => $value) {
         $num++;
         // offset $num to add one and start list at 1
         //$num = $num + 1;
@@ -42,14 +41,40 @@ function list_items($list)
         return $list_string;
 }
 
+function srt_type($input, $items){
+    switch ($input) {
+        case 'a':
+            asort($items);
+            break;
+
+        case 'z':
+            arsort($items);
+            break; 
+
+        case 'o':
+            ksort($items);
+            break;
+
+        case 'r':
+            krsort($items);
+            break;  
+    }
+    return $items;
+    //this returns the newly sorted $items according to the type chosen.
+  }  
+  
+        
+
 
  // The loop!
  do {
      // Echo the list produced by the function
-     echo list_items($items);
+     // echo list_items(($items))
+    echo list_items(($items));
+     
 
       // Show the menu options
-     echo '(N)ew item, (R)emove item, (Q)uit : ';
+     echo '(N)ew item, (R)emove item, (Q)uit, (S)ort : ';
 
          // Get the input from user
      // Use trim() to remove whitespace and newlines
@@ -57,7 +82,7 @@ function list_items($list)
 
 
 
-     // Check for actionable input
+     
 if ($input == 'N') 
 {
          // Ask for entry
@@ -65,6 +90,22 @@ if ($input == 'N')
          // Add entry to list array
         $items[] = get_input();
 	} 
+
+elseif ($input == 'S') {
+
+     echo '(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: '; 
+      
+      //get sort type
+      $input = get_input();
+
+      //call in $items (this is the array) and $input (the sort type chosen) into sort function
+      $items = srt_type($input,$items);
+      //make it equal to $items - this now redefines $items as the newly sorted array.
+      $items = array_values($items);
+      //this ensures that numbering of items on list is numeric.
+    }
+        
+
 elseif ($input == 'R') {
          // Remove which item?
          echo 'Enter item number to remove: ';
@@ -75,11 +116,12 @@ elseif ($input == 'R') {
          
          $items = array_values($items);
 	}
+
  	// Exit when input is (Q)uit
 } while ($input != 'Q');
  
 
- 	// Say Goodbye!
+ 	// Say Goodbye
  	echo "Goodbye!\n";
 
  	// Exit with 0 errors
