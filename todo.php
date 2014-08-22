396fd638f0211bbbb2acf1935f716fbb434563a5
Ruth Spina <ruthspina@Ruths-MacBook-Air.local>
2014-08-22 08:18:47 -0500

    updated list

--- todo.php
+++ todo.php
@@ -6,9 +6,10 @@ $items = array();
 //open array to accomodate the items added/removed.
  
 
+
+function get_input($upper = FALSE) 
  // Get STDIN (end user input) strip whitespace and newlines, 
  // and convert to uppercase if $upper is true
-function get_input($upper = FALSE) 
 {
 
     if ($upper) {
@@ -41,6 +42,7 @@ foreach($list as $num => $value) {
         return $list_string;
 }
 
+
 function srt_type($input, $items){
     switch ($input) {
         case 'a':
@@ -72,6 +74,7 @@ function srt_type($input, $items){
         if($input == 'B'){
             echo 'Enter item: ';
             array_unshift($items, get_input());
+
         }else{
             echo 'Enter item: ';
             array_push($items, get_input());
@@ -79,44 +82,85 @@ function srt_type($input, $items){
 
         return $items;
   }
+
   
-        
 
 
- // The loop!
+function update_file($file_input, $items){
+    var_dump($file_input);
+    $handle = fopen($file_input, 'r');
+    $content = trim(fread($handle, filesize($file_input)));
+    fclose($handle); 
+    $new_list = explode("\n", $content);
+    $items = array_merge($new_list, $items);
+    
+    return $items;
+}  
+
+function save_input($file_path, $items){
+    $file_save = file_exists($file_path);
+    if($file_save) {
+        echo "Type (Y) to confirm save or (X) to return to list" . PHP_EOL;
+        $input = strtoupper(trim(fgets(STDIN)));
+        if($input == 'X') {
+             return;
+        }
+    } else {
+        $handle = fopen($file_path, 'w');
+        $items = implode("\n", $items);
+        fwrite($handle, $items);  
+        echo "FILE NOT FOUND, but was created and saved!" . PHP_EOL . PHP_EOL;
+  
+    }
+    }     
+
+            
+   
+
+/////// The loop!/////
  do {
      // Echo the list produced by the function
-     // echo list_items(($items))
-    echo list_items(($items)) . PHP_EOL;
-     
+     echo list_items($items). PHP_EOL;  
 
       // Show the menu options
-     echo '(N)ew item, (R)emove item, (Q)uit, (S)ort : ';
+     echo '(N)ew item, (R)emove item, (Q)uit, (S)ort, (O)pen, s(A)ve : ';
 
          // Get the input from user
      // Use trim() to remove whitespace and newlines
      $input = get_input(TRUE);
 
 switch ($input){
+    case 'A':
+      echo "Enter filepath: " . PHP_EOL;
+      $file_path = get_input();
+      save_input($file_path, $items);
+      break;
+
+
+    case 'O':
+      echo "Enter filepath: " . PHP_EOL;
+        $file_input = get_input();
+        $items = update_file($file_input, $items);
+        break;
+
+
     case 'N':
 
         $items = beg_end($items);
-        
          // Add entry to list array
-    break;
+        break;
         
     case 'S':
 
-     echo '(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: '; 
-      
-      //get sort type
-      $input = get_input();
+         echo '(A)-Z, (Z)-A, (O)rder entered, (R)everse order entered: '; 
+          //get sort type
+          $input = get_input();
 
-      //call in $items (this is the array) and $input (the sort type chosen) into sort function
-      $items = srt_type($input,$items);
-      //make it equal to $items - this now redefines $items as the newly sorted array.
-      $items = array_values($items);
-      //this ensures that numbering of items on list is numeric.
+          //call in $items (this is the array) and $input (the sort type chosen) into sort function
+          $items = srt_type($input,$items);
+          //make it equal to $items - this now redefines $items as the newly sorted array.
+          $items = array_values($items);
+          //this ensures that numbering of items on list is numeric.
     break;
 
     case 'F':
